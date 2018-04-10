456aff07c8aa190a76d8c64c15b8398c1c527ab3
mrfreedeer <31293109+mrfreedeer@users.noreply.github.com>
2018-04-10 16:47:50 -0500

    Update SEU.vhd

--- SEU.vhd
+++ SEU.vhd
@@ -1,10 +1,25 @@
+----------------------------------------------------------------------------------
+-- Company: iMacLinDows 
+-- Engineers:	Juan Pablo Ospina Bustamante 
+--	   	John Sebastián Luján Figueroa
+-- 
+-- Create Date:    16:13:07 04/10/2018 
+-- Design Name: 	 SEU File Design
+-- Module Name:    SEU - Behavioral 
+-- Project Name: 	 First Processor
+
+--
+----------------------------------------------------------------------------------
+
+
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 
 entity SEU is
-    Port ( imm13 : in  STD_LOGIC_VECTOR (12 downto 0);
-				exto : out STD_LOGIC_VECTOR (31 downto 0));
+    Port ( 	imm13 : in  STD_LOGIC_VECTOR (12 downto 0);
+		exto : out STD_LOGIC_VECTOR (31 downto 0));
 
 end SEU;
 
@@ -12,7 +27,7 @@ architecture Behavioral of SEU is
 
 begin
 exto <= "1111111111111111111" & imm13 when imm13(12)='1' else
-			"0000000000000000000" & imm13 when imm13(12)='0';
+"0000000000000000000" & imm13 when imm13(12)='0';
 
 end Behavioral;
 
