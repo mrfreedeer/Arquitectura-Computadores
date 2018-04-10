59415c48c6d07341851993dbc6f3259276375945
mrfreedeer <31293109+mrfreedeer@users.noreply.github.com>
2018-04-10 16:55:30 -0500

    Add files via upload
    Register File Files

---
+++ RegisterFile.vhd
@@ -0,0 +1,59 @@
+----------------------------------------------------------------------------------
+-- Company: iMacLinDows 
+-- Engineers: Juan Pablo Ospina Bustamante 
+--				  John Sebastián Luján Figueroa
+-- 
+-- Create Date:    16:13:07 04/10/2018 
+-- Design Name: 	 Register File Design
+-- Module Name:    RegisterFile - Behavioral 
+-- Project Name: 	 First Processor
+
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity RegisterFile is
+    Port ( rs1 : in  STD_LOGIC_VECTOR (4 downto 0);
+           rs2 : in  STD_LOGIC_VECTOR (4 downto 0);
+           rd : in  STD_LOGIC_VECTOR (4 downto 0);
+           DWR : in  STD_LOGIC_VECTOR (31 downto 0);
+           reset : in  STD_LOGIC;
+           CRS1 : out  STD_LOGIC_VECTOR (31 downto 0);
+           CRS2 : out  STD_LOGIC_VECTOR (31 downto 0));
+end RegisterFile;
+
+architecture Behavioral of RegisterFile is
+
+type ram_type is array(0 to 31) of std_logic_vector (31 downto 0);
+
+signal RF: ram_type := (others => x"00000000");
+begin
+	process(reset, rs1, rs2, rd, DWR)
+		begin
+		if (reset = '1') then
+			RF <= (others => X"00000000");
+			CRS1 <=  (others => '0');
+			CRS2 <=  (others => '0');		
+		else
+			CRS1 <= RF(conv_integer(rs1));
+			CRS2 <= RF(conv_integer(rs2));
+			if (rd /= "00000") then
+				RF(conv_integer(rd)) <= DWR;
+			end if;
+		end if;
+		
+	end process;
+
+end Behavioral;
+
---
+++ Tb_RegisterFile.vhd
@@ -0,0 +1,108 @@
+----------------------------------------------------------------------------------
+-- Company: iMacLinDows 
+-- Engineers: Juan Pablo Ospina Bustamante 
+--				  John Sebastián Luján Figueroa
+-- 
+-- Create Date:    16:13:07 04/10/2018 
+-- Design Name: 	 Register File Design
+-- Module Name:    Test Bench RegisterFile - Behavioral 
+-- Project Name: 	 First Processor
+
+--
+----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY Tb_RegisterFile IS
+END Tb_RegisterFile;
+ 
+ARCHITECTURE behavior OF Tb_RegisterFile IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT RegisterFile
+    PORT(
+         rs1 : IN  std_logic_vector(4 downto 0);
+         rs2 : IN  std_logic_vector(4 downto 0);
+         rd : IN  std_logic_vector(4 downto 0);
+         DWR : IN  std_logic_vector(31 downto 0);
+         reset : IN  std_logic;
+         CRS1 : OUT  std_logic_vector(31 downto 0);
+         CRS2 : OUT  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal rs1 : std_logic_vector(4 downto 0) := (others => '0');
+   signal rs2 : std_logic_vector(4 downto 0) := (others => '0');
+   signal rd : std_logic_vector(4 downto 0) := (others => '0');
+   signal DWR : std_logic_vector(31 downto 0) := (others => '0');
+   signal reset : std_logic := '0';
+
+ 	--Outputs
+   signal CRS1 : std_logic_vector(31 downto 0);
+   signal CRS2 : std_logic_vector(31 downto 0);
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: RegisterFile PORT MAP (
+          rs1 => rs1,
+          rs2 => rs2,
+          rd => rd,
+          DWR => DWR,
+          reset => reset,
+          CRS1 => CRS1,
+          CRS2 => CRS2
+        );
+
+
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+		reset <= '1';
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+		reset <= '0';
+		rd <= "11111";
+		DWR <= X"0D00C000";
+		
+		wait for 100 ns;	
+		
+		rd <= "00111";
+		DWR <= X"00ACAA00";
+		
+		wait for 100 ns;	
+		
+		rs1 <= "11111";
+		rs2 <= "00111";
+		
+		wait for 100 ns;	
+		
+		rd <= "00000";
+		DWR <= X"FFFFFFFF";
+		
+		wait for 100 ns;	
+		
+		rs1 <= "00000";
+		rs2 <= "00111";
+		
+		wait for 100 ns;	
+		
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
