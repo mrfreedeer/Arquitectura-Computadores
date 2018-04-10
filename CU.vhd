b297830bc2b479917f4ee9fe94acd15f946e1675
mrfreedeer <31293109+mrfreedeer@users.noreply.github.com>
2018-04-10 17:10:40 -0500

    Add files via upload
    CU Files

---
+++ CU.vhd
@@ -0,0 +1,56 @@
+----------------------------------------------------------------------------------
+-- Company: iMacLinDows 
+-- Engineers: Juan Pablo Ospina Bustamante 
+--				  John Sebastián Luján Figueroa
+-- 
+-- Create Date:    17:06:01 04/10/2018
+-- Design Name: 	 CU File Design
+-- Module Name:    CU - Behavioral 
+-- Project Name: 	 First Processor
+
+--
+
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity CU is
+    Port ( OP : in  STD_LOGIC_VECTOR (1 downto 0);
+           OP3 : in  STD_LOGIC_VECTOR (5 downto 0);
+		   ALUOP : out  STD_LOGIC_VECTOR (5 downto 0));
+end CU;
+
+architecture Behavioral of CU is
+
+begin
+
+process(OP, OP3) begin
+	case OP is
+		when "01" =>
+			case OP3 is
+				when "000000" =>
+					ALUOP <= OP3;
+			   when "000001" =>
+					ALUOP <= OP3;
+				when "000100" =>
+					ALUOP <= OP3;
+				when "000010" =>
+					ALUOP <= OP3;
+				when "000011" =>
+					ALUOP <= OP3;
+				when "000111" =>
+					ALUOP <= OP3;
+				when "000101" =>
+					ALUOP <= OP3;
+				when "000110" =>
+					ALUOP <= OP3;
+				when others =>
+					ALUOP <= OP3;
+		end case;
+		when others =>
+		ALUOP <= "111111";
+	end case;
+end process;
+
+end Behavioral;
---
+++ Tb_CU.vhd
@@ -0,0 +1,105 @@
+----------------------------------------------------------------------------------
+-- Company: iMacLinDows 
+-- Engineers: Juan Pablo Ospina Bustamante 
+--				  John Sebastián Luján Figueroa
+-- 
+-- Create Date:    17:06:40 04/10/2018
+-- Design Name: 	 Tb_CU File Design
+-- Module Name:    Tb_CU - Behavioral 
+-- Project Name: 	 First Processor
+
+--
+
+----------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY Tb_CU IS
+END Tb_CU;
+ 
+ARCHITECTURE behavior OF Tb_CU IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT CU
+    PORT(
+         OP : IN  std_logic_vector(1 downto 0);
+         OP3 : IN  std_logic_vector(5 downto 0);
+         ALUOP : OUT  std_logic_vector(5 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal OP : std_logic_vector(1 downto 0) := (others => '0');
+   signal OP3 : std_logic_vector(5 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal ALUOP : std_logic_vector(5 downto 0);
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: CU PORT MAP (
+          OP => OP,
+          OP3 => OP3,
+          ALUOP => ALUOP
+        );
+
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+	
+		OP <= "00";
+      -- hold reset state for 100 ns.
+      wait for 100 ns;
+
+		
+		OP <= "01";
+		OP3 <= "000000";
+      wait for 100 ns;	
+		
+		OP <= "01";
+		OP3 <= "000001";
+      wait for 100 ns;	
+		
+		OP <= "01";
+		OP3 <= "000010";
+      wait for 100 ns;	
+		
+		OP <= "01";
+		OP3 <= "000011";
+      wait for 100 ns;	
+		
+		OP <= "01";
+		OP3 <= "000100";
+      wait for 100 ns;	
+		
+		OP <= "01";
+		OP3 <= "000101";
+      wait for 100 ns;	
+		
+		OP <= "01";
+		OP3 <= "000110";
+      wait for 100 ns;	
+
+		OP <= "01";
+		OP3 <= "000111";
+      wait for 100 ns;	
+
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
