11b92b97c418ebbc4739985c4fbb3e64a4ee3aaf
mrfreedeer <31293109+mrfreedeer@users.noreply.github.com>
2018-04-15 13:05:31 -0500

    Update MUX.vhd

--- MUX.vhd
+++ MUX.vhd
@@ -15,10 +15,10 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity MUX is
-    Port ( i : in  STD_LOGIC;
-				CRS2 : in  STD_LOGIC_VECTOR (31 downto 0);
-           IMM : in  STD_LOGIC_VECTOR (31 downto 0);
-           RMUX : out  STD_LOGIC_VECTOR (31 downto 0));
+    Port ( 	i : in  STD_LOGIC;
+		CRS2 : in  STD_LOGIC_VECTOR (31 downto 0);
+           	IMM : in  STD_LOGIC_VECTOR (31 downto 0);
+           	RMUX : out  STD_LOGIC_VECTOR (31 downto 0));
 end MUX;
 
 architecture Behavioral of MUX is
